b65797f98f0429c960a64e93766dbe023b6be1d8
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-04-01 07:11:16 -0400

    Funciones en Javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,13 +1,29 @@
 var d = document.getElementById('dibujito');
 var lienzo = d.getContext('2d');
+var lineas = 30, y = 0, x = 10, l = 0;
+var colorcito = "#FAA"
 
-//empieza el camino
-lienzo.beginPath();
-//establece el estilo de la raya
-lienzo.strokeStyle = 'blue';
-lienzo.moveTo(50, 10);
-lienzo.lineTo(250, 2);
-//traza la raya
-lienzo.stroke();
-//finaliza el camino
-lienzo.closePath();
+for(l = 0; l < lineas; l+=1){
+  y = l * 10;
+  x = (l + 1) * 10;
+  dibujarLinea(colorcito, 0, y, x, 300);
+  dibujarLinea(colorcito, x, 0, 300, y);
+}
+
+dibujarLinea(colorcito, 1, 1, 299, 1);
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
+dibujarLinea(colorcito, 299, 1, 299, 299);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
+  //empieza el camino
+  lienzo.beginPath();
+  //establece el estilo de la raya
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  //traza la raya
+  lienzo.stroke();
+  //finaliza el camino
+  lienzo.closePath();
+}
