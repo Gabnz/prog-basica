da2bed6de160849b1c416c64448b230f18ed16dd
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-04-08 09:09:49 -0400

    Uso y carga de imagenes en Canvas

--- villaplatzi/platzi.js
+++ villaplatzi/platzi.js
@@ -1,15 +1,116 @@
-var z;
-var n = 10;
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
 
-for(var i = 0; i < n; ++i)
+var fondo = {
+  url: "tile.png",
+  cargaOK: false
+};
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+var vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+var cantidadVacas = aleatorio (1, 10);
+
+var pollo = {
+  url: "pollo.png",
+  cargaOK: false
+};
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
+var cantidadPollos = aleatorio (3, 15);
+
+var cerdo = {
+  url: "cerdo.png",
+  cargaOK: false
+};
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+var cantidadCerdos = aleatorio (5, 9);
+
+function cargarFondo ()
 {
-	z = aleatorio(10, 20);
-	document.write(z);
+  fondo.cargaOK = true;
+  dibujar();
+}
 
-	if(i < n - 1)
-	{
-		document.write(", ");
-	}
+function cargarVacas ()
+{
+  vaca.cargaOK = true;
+  dibujar();
+}
+
+function cargarPollos ()
+{
+  pollo.cargaOK = true;
+  dibujar();
+}
+
+function cargarCerdos ()
+{
+  cerdo.cargaOK = true;
+  dibujar();
+}
+
+function dibujar ()
+{
+  if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+
+  var x, y;
+
+  if(vaca.cargaOK)
+  {
+    for(var v = 0; v < cantidadVacas; v++)
+    {
+      x = aleatorio(0, 7);
+      y = aleatorio(0, 7);
+      x = x * 60;
+      y = y * 60;
+      papel.drawImage(vaca.imagen, x, y);
+    }
+  }
+
+  if(pollo.cargaOK)
+  {
+    for(var p = 0; p < cantidadPollos; p++)
+    {
+      x = aleatorio(0, 7);
+      y = aleatorio(0, 7);
+      x = x * 60;
+      y = y * 60;
+      papel.drawImage(pollo.imagen, x, y);
+    }
+  }
+
+  if(cerdo.cargaOK)
+  {
+    for(var c = 0; c < cantidadCerdos; c++)
+    {
+      x = aleatorio(0, 7);
+      y = aleatorio(0, 7);
+      x = x * 60;
+      y = y * 60;
+      papel.drawImage(cerdo.imagen, x, y);
+    }
+  }
 }
 
 function aleatorio (min, maxi)
